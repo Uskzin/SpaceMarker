0a3f13f0121940017b916155072ac2910faa7d0b
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-21 16:37:26 -0300

    V.1

---
+++ main.py
@@ -0,0 +1,8 @@
+import pygame
+import winsound
+pygame.init()
+
+tamanho = (800,600)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("SpaceMarker")
+fundo = pygame.image.load("bg.jpg")
