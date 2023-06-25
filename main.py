8042ab7c5827d3b53fb3c7ec191607fbe4292244
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-25 10:41:35 -0300

    Cores

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ janela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("SPACE MARKER")
 fundo = pygame.image.load("imagem.fundo.jpg")
 fundo = pygame.transform.scale(fundo, (largura, altura))
+pygame.mixer.music.load("Space_Machine_Power.mp3")
+pygame.mixer.music.play(-1) 
+icone = pygame.image.load("space.png")
+pygame.display.set_icon(icone)
+
