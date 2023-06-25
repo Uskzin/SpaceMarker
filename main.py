f6beb2bedeb9afd2089ba91fef212378a762e276
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-25 18:44:14 -0300

    Criando Defs

--- main.py
+++ main.py
@@ -18,3 +18,37 @@ branco=(255,255,255)
 circulos=[]
 estrelas = []
 sys = SystemExit
+
+#DEFS
+
+def carregar_pontos(filename):
+    global estrelas, circulos
+    estrelas = []
+    circulos = []
+    with open(filename, 'r') as file:
+        for line in file:
+            data = line.strip()
+            name_start = data.index(' ') + 1
+            name_end = data.index('(')
+            name = data[name_start:name_end].strip()
+            coords_start = data.index('(') + 1
+            coords_end = data.index(')')
+            coords = data[coords_start:coords_end].strip().split(',')
+            pos = (int(coords[0]), int(coords[1]))
+            estrelas.append((pos, name))
+            circulos.append(pos)
+
+def calcular_distancia(p1, p2):
+    distancia_x = abs(p2[0] - p1[0])
+    distancia_y = abs(p2[1] - p1[1])
+    return distancia_x + distancia_y
+
+def salvamento_de_pontos(filename):
+    with open(filename, 'w') as file:
+        for pos, name in estrelas:
+            file.write(f"{name} ({pos[0]}, {pos[1]})\n")
+
+def reset_de_marcações():
+    global estrelas,circulos
+    estrelas=[]
+    circulos=[]         
