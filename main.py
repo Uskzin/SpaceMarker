8b8e26ab8207e945b69cfd245f54f815262fdb07
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-25 10:37:07 -0300

    Sons e Fundo

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from tkinter import simpledialog
 import winsound
 
 pygame.init()
-
-tamanho = (800,600)
-tela = pygame.display.set_mode(tamanho)
-pygame.display.set_caption("SpaceMarker")
-fundo = pygame.image.load("bg.jpg")
+sys = SystemExit
+largura = 800
+altura = 600
+janela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("SPACE MARKER")
+fundo = pygame.image.load("imagem.fundo.jpg")
+fundo = pygame.transform.scale(fundo, (largura, altura))
