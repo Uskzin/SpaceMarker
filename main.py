93aa24cb9ab283e7f9a8c7dd57378e6da2611742
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-25 18:38:37 -0300

    Reforma Codigo, comandos básicos

--- main.py
+++ main.py
@@ -5,50 +5,16 @@ from tkinter import simpledialog
 import winsound
 
 pygame.init()
-sys = SystemExit
-largura = 800
-altura = 600
-janela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("SPACE MARKER")
+tamanho = (800, 600)
+tela = pygame.display.set_mode(tamanho)
+pygame.display.set_caption("Space Marker")
+icon=pygame.image.load("space.png")
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
 fundo = pygame.image.load("imagem.fundo.jpg")
-fundo = pygame.transform.scale(fundo, (largura, altura))
 pygame.mixer.music.load("Space_Machine_Power.mp3")
-pygame.mixer.music.play(-1) 
-icone = pygame.image.load("space.png")
-pygame.display.set_icon(icone)
-branco = (255, 255, 255)
-vermelho = (255, 0, 0) 
-marcacoes = {}
-linhas = []
-distancias = []
-pontos = []
-"Funçoes"
-def obter_nome_estrela():
-    root = tk.Tk()
-    root.withdraw()
-    nome = simpledialog.askstring("Nome da Estrela", "Digite o nome da estrela:")
-    return nome
-
-try:
-    while True:
-         for evento in pygame.event.get():
-             if evento.type == pygame.MOUSEBUTTONDOWN:
-                 if evento.button == 1:
-                    posicao_mouse = pygame.mouse.get_pos()
-                    pontos.append(posicao_mouse)
-                    nome_estrela = obter_nome_estrela()
-                    marcacoes[posicao_mouse] = nome_estrela
-                    janela.blit(fundo, (0, 0))
-
-
-
-
-
-
-
-    
-except Exception as erro:
-    # Lidar com erros
-     print(f"Ocorreu um erro: {erro}")
-     pygame.quit()
-     sys()
+pygame.mixer.music.play(0,0,5)
+branco=(255,255,255)
+circulos=[]
+estrelas = []
+sys = SystemExit
