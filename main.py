8b0ce91b7ac8e8467fb998ef981f107fff0be03e
Pablo Antônio De Oliveira <pablodeoliveiracontato@gmail.com>
2023-06-25 11:11:39 -0300

    Adicionando While, try, except

--- main.py
+++ main.py
@@ -21,9 +21,34 @@ vermelho = (255, 0, 0)
 marcacoes = {}
 linhas = []
 distancias = []
+pontos = []
 "Funçoes"
 def obter_nome_estrela():
     root = tk.Tk()
     root.withdraw()
     nome = simpledialog.askstring("Nome da Estrela", "Digite o nome da estrela:")
     return nome
+
+try:
+    while True:
+         for evento in pygame.event.get():
+             if evento.type == pygame.MOUSEBUTTONDOWN:
+                 if evento.button == 1:
+                    posicao_mouse = pygame.mouse.get_pos()
+                    pontos.append(posicao_mouse)
+                    nome_estrela = obter_nome_estrela()
+                    marcacoes[posicao_mouse] = nome_estrela
+                    janela.blit(fundo, (0, 0))
+
+
+
+
+
+
+
+    
+except Exception as erro:
+    # Lidar com erros
+     print(f"Ocorreu um erro: {erro}")
+     pygame.quit()
+     sys()
