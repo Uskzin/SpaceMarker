339d9ba8d657905baf921b49b824fe367a9ef0f3
Pablo Oliveira <uskzin6632@gmail.com>
2023-06-27 09:53:25 -0300

    Inicialização e configs gerais

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+import math
+import tkinter as tk
+from tkinter import simpledialog
+
+# Inicialização do Pygame
+pygame.init()
+
+sys = SystemExit
+# Configuração da janela
+largura = 800
+altura = 600
+janela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("SPACE MARKER")
+tamanho_tela = (largura, altura)
+
+# Carregamento do som de fundo
+pygame.mixer.music.load("Space_Machine_Power.mp3")
+pygame.mixer.music.play(-1)  # Reproduzir em looping
+
+# Carregamento do ícone
+icone = pygame.image.load("space.png")
+pygame.display.set_icon(icone)
+
+# Definição das cores
+branco = (255, 255, 255)
+vermelho = (255, 0, 0)
